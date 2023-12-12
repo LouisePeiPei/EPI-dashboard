1c6e86dedce96edb124b05e4ffa8f661ad465955
LouisePeiPei <luyaopei@brandeis.edu>
2023-12-12 16:18:33 -0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/EPI-dashboard/",
+  base: '/EPI-dashboard/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        dir: 'dist',
+      },
+    },
+    assets: {
+      // Adjust the path to match the location of your CSV file within the 'data' folder
+      include: ['data/per-capita-emissions.csv'],
+      // Modify the destination path if needed
+      // This will place the copied file in the 'data' folder within the build output
+      base: '/EPI-dashboard/data',
+    },
+  },
+});
 
-})
